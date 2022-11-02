48d46820b8bc268ba679f6dd45357d19c2953ccb
Pedro <pedrohvidals@gmail.com>
2022-11-02 20:41:38 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function github(){
+    document.getElementById("github").src = "images/github_logo_tr.png";
+}
+
+function github_out(){
+    document.getElementById("github").src = "images/github_logo.png";
+}
+
+function linkedin(){
+    document.getElementById("linkedin").src = "images/linkedin_logo_tr.png";
+}
+
+function linkedin_out(){
+    document.getElementById("linkedin").src = "images/linkedin_logo.png";
+}
+
+function insta(){
+    document.getElementById("insta").src = "images/instagram_logo_tr.png";   
+}
+
+function insta_out(){
+    document.getElementById("insta").src = "images/instagram_logo.png";   
+}
